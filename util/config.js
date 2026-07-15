bca53361e16f7457bd79678dbafa09dcf90169ca
jsahlsa <joe.sahlstrom@gmail.com>
2026-07-15 12:06:46 -0700

    looks like all tests are passing

--- util/config.js
+++ util/config.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
-  TEST_DATABASE_URL: process.env.DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   PORT: process.env.PORT || 3001,
   SECRET: 'jfkdjfs88fd8f9d8fdjfjdfdfd88833udue9398',
+  TESTING: process.env.TESTING,
 }
--- util/db.js
+++ util/db.js
@@ -1,15 +1,9 @@
 const Sequelize = require('sequelize')
-const { DATABASE_URL, TEST_DATABASE_URL } = require('./config')
+const { DATABASE_URL, TEST_DATABASE_URL, TESTING } = require('./config')
 
-let database
+const dbUrl = TESTING === 'true' ? TEST_DATABASE_URL : DATABASE_URL
 
-if (process.env.TESTING) {
-  database = TEST_DATABASE_URL
-} else {
-  database = DATABASE_URL
-}
-
-const sequelize = new Sequelize(database, {
+const sequelize = new Sequelize(dbUrl, {
   dialectOptions: {
     ssl: {
       require: true,
@@ -21,7 +15,7 @@ const sequelize = new Sequelize(database, {
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
-    console.log('connected to database')
+    console.log('connected to database: ', TESTING === 'true' ? 'test-db' : 'defaultdb')
   } catch (err) {
     console.log('failed to connect to database')
     return process.exit(1)
